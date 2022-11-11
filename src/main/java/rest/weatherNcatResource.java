4d63ab89fbfe233c201f150414cf5fb16df0db75
michaelxuw <michaelxuwrelated@gmail.com>
2022-11-11 13:20:16 +0100

    combined api1 and api2 to weatherNcat

--- src/main/java/rest/weatherNcatResource.java
+++ src/main/java/rest/weatherNcatResource.java
@@ -3,44 +3,45 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dtos.catDTO;
+import dtos.weatherCatDTO;
 import dtos.weatherDTO;
-import entities.User;
-import utils.EMF_Creator;
 import utils.HttpUtils;
 
-import javax.annotation.security.RolesAllowed;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
 
 /**
  * @author lam@cphbusiness.dk
  */
-@Path("api2")
-public class api2Resource {
+@Path("weatherNcat")
+public class weatherNcatResource {
 
     @Context
     private UriInfo context;
-    private String urlBase = "https://vejr.eu/api.php?location=Koebenhavn&degree=C";
-    private String urlGet = "https://vejr.eu/api.php?location=x&degree=C";
+    private String urlCat = "https://api.thecatapi.com/v1/images/search";
+    private String urlWeatherBase = "https://vejr.eu/api.php?location=Koebenhavn&degree=C";
+    private String urlWeatherGet = "https://vejr.eu/api.php?location=x&degree=C";
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getCopenhagenWeather() throws IOException {
+    public Response getCatUrl() throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String res = HttpUtils.fetchData(urlBase);
-        weatherDTO weather = gson.fromJson(res, weatherDTO.class);
-        //This is what your endpoint should return
-        String weatherData = gson.toJson(weather);
 
-        return weatherData;
+        String res = HttpUtils.fetchData(urlCat);
+        catDTO[] catDTO = new Gson().fromJson(res, catDTO[].class);
+
+        res = HttpUtils.fetchData(urlWeatherBase);
+        weatherDTO weather = gson.fromJson(res, weatherDTO.class);
+
+        weatherCatDTO weatherCat = new weatherCatDTO(weather, catDTO[0]);
+        String result = gson.toJson(weatherCat);
+
+        return Response.ok().entity(result).build();
     }
 
     @POST
@@ -51,12 +52,13 @@ public class api2Resource {
         Object city = gson.fromJson(content, Object.class);
         String cityName = city.toString().substring(6,city.toString().length()-1);
 
-        String url = urlGet.replace("x", cityName);
+        String url = urlWeatherGet.replace("x", cityName);
         String res = HttpUtils.fetchData(url);
         weatherDTO weather = gson.fromJson(res, weatherDTO.class);
         String weatherData = gson.toJson(weather);
 
         return weatherData;
     }
+
 
 }
---
+++ src/test/java/rest/api1ResourceTest.java
@@ -0,0 +1,71 @@
+package rest;
+
+import entities.Role;
+import entities.User;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.*;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class api1ResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api/";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+
+        httpServer.shutdownNow();
+    }
+
+    @Test
+    public void serverIsRunning() {
+        given().when().get("/info").then().statusCode(200);
+    }
+
+
+    @Test
+    void getCatUrl() {
+        given()
+                .contentType("application/json")
+                .when()
+                .get("/api1/").then()
+                .statusCode(200)
+                .body("msg", containsString("https://cdn2.thecatapi.com/images/"));
+    }
+
+
+}
